d8b9716a33820b193776369b2ccac0822ba3b62f
itsnestor <nestor.dejoya.campaner@gmail.com>
2021-04-29 16:06:43 -0400

    added initial start function

--- index.js
+++ index.js
@@ -12,9 +12,59 @@ const connection = mysql.createConnection({
   
     password: 'Nest0r526!',
     database: 'employeTracker_DB',
-  });
+});
 
-  connection.connect((err) => {
+connection.connect((err) => {
     if (err) throw err;
     start();
-  });
+});
+
+const start = () => {
+    inquirer
+    .prompt({
+        name: 'action',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+            'View All Employees',
+            'View All Employees by Department',
+            'View All Employees by Manager',
+            'Add Employee',
+            'Remove Employee',
+            'Update Employee Role',
+            'Update Employee Manager',
+        ],
+    })
+    .then((answer) => {
+        switch (answer.action) {
+            case 'View All Employees':
+                viewAllEmployees();
+                break;
+
+            case 'View All Employees by Department':
+                viewByDepartment();
+                break;
+            
+            
+            case 'View All Employees by Manager':
+                viewByManager();
+                break;
+        
+            case 'Add Employee':
+                addEmployee();
+                break;
+
+            case 'Remove Employee':
+                removeEmployee();
+                break;
+
+            case 'Update Employee Role':
+                updateRole();
+                break;
+
+            case 'Update Employee Manager':
+                updateManager();
+                break;
+        }
+    })
+}
